d6ced87c8ffdfaa29707a8a5fe193692cb0e6ca6
Evan Edge <evanjedge@gmail.com>
2016-04-18 16:33:33 -0700

    Fixed bootstrap issues. Working animations, but not all fields completed.

--- hello_world/app/assets/javascripts/application.js
+++ hello_world/app/assets/javascripts/application.js
@@ -14,10 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require animations.min.js
-//= require appear.min.js
-//= require bootstrap.js
-//= require custom-solid.js
 //= require jquery-1.11.1.js
 //= require jquery.isotope.js
+//= require appear.min.js
+//= require animations.min.js
+//= require bootstrap.js
+//= require custom-solid.js
 //= require jquery.vegas.min.js
+//= require jquery.fancybox.js
+//= require jquery.fancybox.pack.js
