de0965f5bb8ea4f681fc05346c9235d4643ec4e9
Monte Thakkar <mthakkar@mail.sfsu.edu>
2016-04-20 12:11:11 -0700

    corrected layouts render, created game view with table and chat window

--- hello_world/config/routes.rb
+++ hello_world/config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+  resources :game
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
