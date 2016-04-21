bda14926e14d70dea9c32ab9413c2e2540ee0bdd
Anurag Nanda <anuragnandasf@gmail.com>
2016-04-20 19:29:10 -0700

    added create, join, index to game, and added some erb in application for testind

--- hello_world/app/controllers/games_controller.rb
+++ hello_world/app/controllers/games_controller.rb
@@ -1,7 +1,31 @@
 class GamesController < ApplicationController
-	def create
-		@game = Game.new
-		
+def create
+	@game = Game.new
+	@game.user_id = params[:id]
+	@game.save
+	render :json => @game
+end
 
-	end
+
+def join
+	@game = Game.find(params[:game_id])
+	@game.user_id = params[:id]
+	@game.save
+	@games = Game.all
+	render :json => @games
+end
+
+
+def index
+	#TODO : this can't be all the games 
+
+	
+	
+	
+ @games = Game.all 
+ render	:json => @games
+end
+
+
+
 end
