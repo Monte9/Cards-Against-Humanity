7081b7d18590a67ef66ebac56ae3ae2d68740251
Anurag Nanda <anuragnandasf@gmail.com>
2016-04-23 13:17:37 -0700

    removed user_id column from games

--- hello_world/app/models/user.rb
+++ hello_world/app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+
+
 end
--- hello_world/db/schema.rb
+++ hello_world/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421004704) do
+ActiveRecord::Schema.define(version: 20160420022735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20160421004704) do
     t.integer  "round_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   create_table "gameusers", force: :cascade do |t|
