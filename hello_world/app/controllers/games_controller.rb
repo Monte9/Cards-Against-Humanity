d71ced6df4d5fa0817b37d7c1607be645da6eb4e
Anurag Nanda <anuragnandasf@gmail.com>
2016-05-03 14:04:17 -0700

    -

--- hello_world/app/controllers/games_controller.rb
+++ hello_world/app/controllers/games_controller.rb
@@ -5,22 +5,47 @@
     #t.datetime "updated_at", null: false
     #t.integer  "user_id"
 
+#create_table "games", force: :cascade do |t|
+    #t.integer  "dealer_id"
+    #t.integer  "round_id"
+    #t.datetime "created_at", null: false
+    #t.datetime "updated_at", null: false
+  #end
 class GamesController < ApplicationController
 
 
 #this create should be called with game_cards create.
+#def create
+	#@game = Game.new
+	#@game.save
+	#render :json => @game
+#end
+
+
+def index offsetV=0
+#TODO : this can't be all the games	
+ @games = Game.limit(20).offset(offsetV) 
+ #render	:json => @games
+end
+
+
 def create
-	@game = Game.new
-	@game.save
-	render :json => @game
+  @game = Game.new(params[:user])
+  	if @game.save
+  		respond_to do |format|
+  			format.js{render :layout => false}
+  			format.html{render "application"}
+  		end
+  	end
 end
 
 
-def index
-	#TODO : this can't be all the games	
- @games = Game.all 
- render	:json => @games
+def delete
+	Game.delete(params[:id])
+	#render 'index'	
 end
+
+
 
 
 
--- hello_world/app/models/user.rb
+++ hello_world/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 	has_many :games
 	has_many :votes
+	has_many :game_users
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
