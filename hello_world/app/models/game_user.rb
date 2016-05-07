d43bc3dea4656284cc67105875d955d7f77e43d1
Anurag Nanda <anuragnandasf@gmail.com>
2016-05-06 19:59:29 -0700

    added all_votes_in?()

--- hello_world/app/models/game_user.rb
+++ hello_world/app/models/game_user.rb
@@ -4,7 +4,7 @@
 class GameUser < ActiveRecord::Base
 	belongs_to :user, :class_name=>"User", :foreign_key=>"user_id"
 	belongs_to :game, :class_name=>"Game", :foreign_key=>"game_id"
-
+	has_many :rounds, through: :game
 
 
 	def username 
--- hello_world/app/models/round.rb
+++ hello_world/app/models/round.rb
@@ -1,6 +1,14 @@
+    #t.integer  "game_id"
+    #t.integer  "card_id" --its the black card id
+    #t.datetime "created_at", null: false
+    #t.datetime "updated_at", null: false
 class Round < ActiveRecord::Base
 	belongs_to :game
 	has_many :cards
 	has_many :votes
-	has_many :game_users
+	has_many :game_users, through: :game
+
+	def all_votes_in?
+		votes.count == game_users.count
+	end
 end
